b0be8d05155d731e5f5914247844d82928ed26b0
DonnaKarina <127282995+DonnaKarina@users.noreply.github.com>
2023-03-15 13:14:40 +0800

    Create variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,20 @@
+#3.1Some simple math
+#Edinburgh's longitude is -3.19
+a=-3.19
+#Los Angeles's longitude is -118.24
+b=-118.24
+#Haining's longitude is 116.39
+c=116.39
+#The longitude distance between Edinburgh and LA is assumed as d
+d=a-b
+#The longitude distance between Edinburgh and Haining is e
+e=c-a
+print d,e
+#e is bigger
+#3.2Booleans
+X = True
+Y = False
+W=X and Y
+Z=X or y
+bool(W)
+bool(Z)
