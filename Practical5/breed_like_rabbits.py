c0509c53e40cc05317b1288cd06647f56e23cb3d
DonnaKarina <127282995+DonnaKarina@users.noreply.github.com>
2023-03-17 16:15:06 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,9 +1,10 @@
 #start with the first generation and the two rabbits
+#use 'while' to ensure the rabbit number is less than 100
+#each couple will pruduce two rabbits, so the number will double each generation
+#at last print the result
 generation=1
 rabbit=2
-#to ensure the rabbit number is less than 100
 while rabbit<100:
-#each couple will pruduce two rabbits, so the number will double each generation
   rabbit=rabbit*2
   generation+=1
 print('In the generation %d'%(generation)+' the water bottle will run out')
