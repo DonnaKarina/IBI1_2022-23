02444c6cb0235379eadaf2350f172a726485bc51
DonnaKarina <127282995+DonnaKarina@users.noreply.github.com>
2023-03-17 16:05:37 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,8 +1,8 @@
 #star with the first generation and the two rabbits
 generation=1
 rabbit=2
-while rabbit<100:
 #to ensure the rabbit number is less than 100
+while rabbit<100:
 #each couple will pruduce two rabbits, so the number will double each generation
   rabbit=rabbit*2
   generation+=1
