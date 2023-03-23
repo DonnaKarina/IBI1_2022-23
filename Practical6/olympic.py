3b7df45e44f3a01a73ce3937a522dd9d7ce85f02
DonnaKarina <127282995+DonnaKarina@users.noreply.github.com>
2023-03-23 20:43:27 +0800

    Update olympic.py

--- Practical6/olympic.py
+++ Practical6/olympic.py
@@ -1,14 +1,17 @@
-#create a list of the cost 
+#create a list of the cost
 #print the list
 cost=[1, 8, 15, 7, 5, 14, 43, 40]
 print(cost)
-#learn from https://matplotlib.org/stable/gallery/lines_bars_and_markers/bar_label_demo.html#sphx-glr-gallery-lines-bars-and-markers-bar-label-demo-py 
+#learn from https://matplotlib.org/stable/gallery/lines_bars_and_markers/bar_la$
 import matplotlib.pyplot as plt
 import numpy as np
-Year = ['1984', '1988', '1992', '1996', '2000', '2003', '2008', '2012']
-cost = [1, 8, 15, 7, 5, 14, 43, 40]
-fig, ax = plt.subplots()
-bar_container = ax.bar(Year, cost)
-ax.set(ylabel='cost', title='Olympic Costs', ylim=(0,45))
-ax.bar_label(bar_container, fmt='{:,.0f}')
+N=8
+cost = (1, 8, 15, 7, 5, 14, 43, 40)
+ind=np.arange(N)
+width=0.4
+pl=plt.bar(ind, cost, width)
+plt.ylable='cost'
+plt.title('Olympic Costs')
+plt.xticks(ind, ('1984', '1988', '1992', '1996', '2000', '2003', '2008', '2012'$
+plt.yticks(np.arange(0, 45, 5))
 plt.show()
