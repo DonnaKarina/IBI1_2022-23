f72bf71a31c50db64d961a12d5f60954f7d7a95a
DonnaKarina <127282995+DonnaKarina@users.noreply.github.com>
2023-03-23 19:12:58 +0800

    Update favourite_movies_genres.py

--- Practical6/favourite_movies_genres.py
+++ Practical6/favourite_movies_genres.py
@@ -1,9 +1,13 @@
 #first create a dictionary according to the data
-#then use pyplot to create a pie chart
+#check if the key is in the dictionary and print it. Learn from https://blog.cs$
+#print the whole dictionary
+#use pyplot to create a pie chart
 #name the labels and then defines their size as the numbers of student
 #label the percentage of each genre
 #ensure the pie is acircle
 genre = {'Comedy':73, 'Action':42, 'Romance':38, 'Fantasy':28, 'Science-fiction$
+print ("genre['Comedy']:", genre['Comedy'])
+print (genre)
 import matplotlib.pyplot as plt
 labels='Comedy', 'Action', 'Romance', 'Fantasy', 'Science-fiction', 'Horror', '$
 sizes=[73, 42, 38, 28, 22, 19, 18, 12, 8, 7]
